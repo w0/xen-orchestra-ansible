fcd066463c071803569ebc938f9fa745042aad9d
w0 <33212583+w0@users.noreply.github.com>
2026-04-19 21:09:29 -0500

    Refactor snapshot filtering and add regex support
    - Change mutually_exclusive to only apply snapshot_name
      and snapshot_uuid
    - Add regex support for snapshot name matching
    - Consolidate snapshot_name and vm_uuid filtering into
      single code path with support for combining filters
    - Add warnings for ignored parameters when snapshot_uuid
      is provided
    - Normalize response to always return a list

--- plugins/modules/xoa_snapshot_info.py
+++ plugins/modules/xoa_snapshot_info.py
@@ -1,3 +1,5 @@
+import re
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.w0.xen_orchestra.plugins.module_utils.xoa_client import (  # type: ignore
     XOAClient,
@@ -32,7 +34,7 @@ def main():
             filter=dict(type="list"),
             limit=dict(type="int"),
         ),
-        mutually_exclusive=["snapshot_name", "snapshot_uuid", "vm_uuid"],
+        mutually_exclusive=[["snapshot_name", "snapshot_uuid"]],
         required_one_of=[["username", "token"]],
         required_together=[["username", "password"]],
         supports_check_mode=True,
@@ -41,43 +43,39 @@ def main():
     client = _client(module)
 
     if module.params["snapshot_uuid"]:
+        ignored = ["snapshot_name", "vm_uuid", "filter", "fields", "limit"]
+
+        for p in ignored:
+            if module.params.get(p):
+                module.warn(f"{p} is ignored when snapshot_uuid is set")
+
         path = module.params["snapshot_uuid"]
         params = None
         if module.params["fields"] != ["uuid", "snapshot_time", "$snapshot_of"]:
             module.warn("fields parameter is ignored when snapshot_uuid is provided")
         if module.params["limit"]:
             module.warn("limit parameter is ignored when snapshot_uuid is provided")
-    elif module.params["snapshot_name"]:
+    else:
         path = None
-        fields = list(module.params["fields"])
-        if "name_label" not in fields:
-            fields.append("name_label")
 
-        filter = f'name_label:"{module.params["snapshot_name"]}"'
+        fields = list(module.params["fields"])
+        filters = list(module.params["filter"] or [])
+
+        if module.params["snapshot_name"]:
+            name = re.escape(module.params["snapshot_name"])
+            filters.append(f"name_label:/^{name}$/")
+            if "name_label" not in fields:
+                fields.append("name_label")
+
+        if module.params["vm_uuid"]:
+            uuid = module.params["vm_uuid"].replace('"', '\\"')
+            filters.append(f'$snapshot_of:"{uuid}"')
+            if "$snapshot_of" not in fields:
+                fields.append("$snapshot_of")
 
         params = dict(
             fields=",".join(fields),
-            filter=filter,
-            limit=module.params["limit"],
-        )
-    elif module.params["vm_uuid"]:
-        path = None
-
-        fields = list(module.params["fields"])
-        if "$snapshot_of" not in fields:
-            fields.append("$snapshot_of")
-
-        filter = f'$snapshot_of:"{module.params["vm_uuid"]}"'
-
-        params = dict(
-            fields=",".join(module.params["fields"]),
-            filter=filter,
-            limit=module.params["limit"],
-        )
-    else:
-        path = None
-        params = dict(
-            fields=",".join(module.params["fields"]),
+            filter=" ".join(filters),
             limit=module.params["limit"],
         )
 
@@ -87,6 +85,9 @@ def main():
         module.fail_json(
             msg="Failed to get snapshot info", result=response, status_code=status_code
         )
+
+    if isinstance(response, dict):
+        response = [response]
 
     module.exit_json(changed=False, snapshots=response)
 
